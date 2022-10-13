c3159f5e9e7d0ae528efae7c46930cf3ab46a266
Adithiyaa  N <114280252+AdithiyaaN@users.noreply.github.com>
2022-10-13 10:28:50 +0000

    Added more dumb fuctions lol

--- set01/problem03.c
+++ set01/problem03.c
@@ -1,3 +1,29 @@
 #include <stdio.h>
-int main(){
-  int a,b}
+int add_numbers(int a,int b);
+int input();
+void output(int a, int b, int sum);
+main()
+{
+  int a,b,sum;
+  a=input();
+  b=input();
+  sum = add_numbers(a,b);
+  output(a,b,sum);
+}
+int add_numbers(int a,int b)
+{
+int add;
+add = a + b;
+return add;  
+  }
+int input()
+{
+  int x;
+  printf("enter the numbers");
+  scanf("%d", &x);
+  return x;
+}
+void output(int a, int b, int x)
+{  
+printf("the sum of %d and %d is %d",a,b,x);
+}
